e05c71ea20c3c670cebd725c80e36c2ecd868a94
Pablo Mendez <mendez.developer@gmail.com>
2021-02-16 19:47:12 -0300

    added sub dir creation test

--- src/test/java/com/salesforce/tests/fs/YourUnitTest.java
+++ src/test/java/com/salesforce/tests/fs/YourUnitTest.java
@@ -1,8 +1,43 @@
 package com.salesforce.tests.fs;
 
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Place holder for your unit tests
  */
-public class YourUnitTest {
+public class YourUnitTest extends BaseTest {
 
+    @Test
+    public void testCreateValidDirectory() {
+        List<String> expectedResults = getExpectedValidDirectories("/root", "/root/new_directory");
+        runTest(toArray(expectedResults), "mkdir new_directory", "ls", "quit");
+    }
+
+    @Test
+    public void testCreateValidDirectoryAndSubDirectories() {
+        List<String> expectedResults = getExpectedValidDirectories(
+                "/root/new_directory/new_directory_lvl2", "/root/new_directory");
+
+        runTest(toArray(expectedResults),
+                "mkdir new_directory",
+                "cd new_directory",
+                "mkdir new_directory_lvl2",
+                "ls",
+                "quit");
+    }
+
+    private List<String> getExpectedValidDirectories(String... values) {
+        List<String> list = new ArrayList<String>();
+        for (String s : values) {
+            list.add(s + "\n");
+        }
+        return list;
+    }
+
+    private String[] toArray(List<String> list) {
+        return list.toArray(new String[0]);
+    }
 }
